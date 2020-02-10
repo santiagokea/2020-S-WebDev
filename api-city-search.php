60a119048ffe4e7408e9056a814e443599a2dbe5
sdathoejdotdk <sd@hoej.dk>
2020-02-10 11:05:11 +0100

    added files to connect to the server via the API

---
+++ api-city-search.php
@@ -0,0 +1,21 @@
+<?php
+http_response_code(200);
+header('Content-Type: application/json');
+$sSearchFor = 'B';
+$sData = file_get_contents('data.json');
+$jData = json_decode($sData);
+$jResponse = new stdClass(); // {}
+$jResponse->cities = [];
+foreach($jData->cities as $jCity){
+  // echo "<div>$jCity->name</div>";
+  if( stripos($jCity->name, $sSearchFor) !== false ){
+    // array_push($jResponse->cities, $jCity);
+    $jResponse->cities[] = $jCity; // same as array_push
+  }
+}
+echo json_encode($jResponse);
+
+
+
+
+
