8b36a30ebf080be444f428746281ef9463572ad0
sdathoejdotdk <sd@hoej.dk>
2020-02-11 10:06:18 +0100

    added json showing flight info

--- Tue-11-Feb/momondo.php
+++ Tue-11-Feb/momondo.php
@@ -3,4 +3,14 @@
 // curl
 // file_get_contents open a file
 // file_get_contents open a URL
-echo file_get_contents('http://localhost/2020-S-WebDev/Tue-11-Feb/sas.php');
+$sData = file_get_contents('http://localhost/2020-S-WebDev/Tue-11-Feb/sas.php');
+//{"status":1,"price":500}
+$jData = json_decode($sData); 
+$jData->price *= 1.1;
+$jData->price = $jData->price * 1.1;
+echo json_encode($jData);
+
+
+
+
+
