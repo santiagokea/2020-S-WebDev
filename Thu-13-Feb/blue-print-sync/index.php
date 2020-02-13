954545c3c731c28261b9837973a7839c2c04b828
sdathoejdotdk <sd@hoej.dk>
2020-02-13 11:24:53 +0100

    added code to get the sync working between client and server with a blueprint

--- Thu-13-Feb/blue-print-sync/index.php
+++ Thu-13-Feb/blue-print-sync/index.php
@@ -13,26 +13,24 @@
     $sBluePrint = file_get_contents('blueprint.html');
     $sCopy = $sBluePrint;
     $sCopy = str_replace('::name::', $jData[0]->name, $sBluePrint);
+    $sCopy = str_replace('::price::', $jData[0]->price, $sCopy);
+    $sCopy = str_replace('::test::', $jData[0]->test, $sCopy);
     echo $sCopy;
     ?>    
   </div>
   <button onclick="getItems()">GET MORE ITEMS</button>
   
-
   <script>
-
     var sBluePrint = `<?= $sBluePrint ?>`
-
-
     async function getItems(){
       var jResponse = await fetch('api-get-items.php')
       var jData = await jResponse.json()
       var sCopy = sBluePrint
       var sItem = sCopy.replace('::name::', jData[1].name)
+      sItem = sItem.replace('::price::', jData[1].price)
+      sItem = sItem.replace('::test::', jData[1].test)
       document.getElementById("items").insertAdjacentHTML('beforeend', sItem)
     }
-
-
   </script>
 
 
