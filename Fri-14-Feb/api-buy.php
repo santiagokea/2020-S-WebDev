41c2e99a3019d066951786eda7764d6be64d6496
sdathoejdotdk <sd@hoej.dk>
2020-02-14 10:51:24 +0100

    send email with a library

--- Fri-14-Feb/api-buy.php
+++ Fri-14-Feb/api-buy.php
@@ -7,9 +7,9 @@ if( !isset($_POST['txtAirlineName']) ||
 }
 
 
-$sFromPhone = '';
-$sToPhone  = '';
-$sApiKey = '';
+$sFromPhone = '25850567';
+$sToPhone  = '25850567';
+$sApiKey = 'IGbfiPPX74MynwibPXq707msvl1AEVfFt2pz36CXy3rIw1cagU';
 $sMessage = urlencode('Thank: '.$_POST['txtAirlineName']);
 if( strlen($sMessage) > 100 ){
   echo 'Message is too long';
