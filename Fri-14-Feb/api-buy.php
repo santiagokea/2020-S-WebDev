77c03a663c486d4e299d39ae4aee66be5107479a
sdathoejdotdk <sd@hoej.dk>
2020-02-14 10:10:00 +0100

    added code for validation

--- Fri-14-Feb/api-buy.php
+++ Fri-14-Feb/api-buy.php
@@ -1,6 +1,20 @@
 <?php
-$sFromPhone = '25850567';
-$sToPhone  = '25850567';
+if( !isset($_POST['txtAirlineName']) ||
+  strlen($_POST['txtAirlineName']) < 2
+){
+  echo 'Airline name must be at least 2 chars';
+  exit();
+}
+
+
+$sFromPhone = '';
+$sToPhone  = '';
+$sApiKey = '';
 $sMessage = urlencode('Thank: '.$_POST['txtAirlineName']);
-$sApiKey = 'IGbfiPPX74MynwibPXq707msvl1AEVfFt2pz36CXy3rIw1cagU';
+if( strlen($sMessage) > 100 ){
+  echo 'Message is too long';
+  exit();
+}
+
 echo file_get_contents("https://fatsms.com/apis/api-send-sms?to-phone=25850567&message=$sMessage&from-phone=25850567&api-key=IGbfiPPX74MynwibPXq707msvl1AEVfFt2pz36CXy3rIw1cagU");
+
